24a99b2df024e6404f178a639371a1b6537f478c
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-16 14:27:20 +0530

    updated code

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,10 @@ const app = express() ;
 const dbConnect = require('./db')
 const PORT = 3000 ;
 
+// const validator = require('validator') ;
+// console.log(validator.isStrongPassword('Hello@123',{
+//      minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, 
+// }))
 //NOTE fn to connect with the mongodb
 dbConnect() ;
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
+const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -7,13 +7,33 @@ const userSchema = new mongoose.Schema({
     maxLength: 40,
   },
   email: {
-    type : String ,
-    required : [true , 'Email is required'],
-    unique : true,
-    validate : {
-        validator :  validator.isEmail ,
-        message : "Please enter a Valid Email"
-    }
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please enter a Valid Email',
+    },
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return validator.isStrongPassword(value, {
+          minLength: 8,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+        });
+      },
+      message: '',
+    },
   },
 });
 
