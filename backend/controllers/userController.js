d3e01871b57a5297cabfec0727d65f2724752ff8
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-18 13:19:21 +0530

    updatedcode

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,22 +2,20 @@ const User = require('./../models/userModel')
 
 exports.register = async(req,res) => {
    try {
-      const {email} = req.body
+      const {email} = req.body 
 const isExistingUser = await User.findOne({email}) ;
 
 if(isExistingUser){
-     res.status(400).send("User already exists")
+  return   res.status(400).send("User already exists")
 }
-
 const user = await User.create(req.body)
-
 if(user){
-     res.status(201).json({
+   return   res.status(201).json({
           message : "User created Successfully",
           data : user
      })
 }
- 
+
    } catch (error) {
      res.status(400).send(error)
    }
