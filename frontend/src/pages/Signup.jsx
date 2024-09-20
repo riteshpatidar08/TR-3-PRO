f48b1ee3617a09d067946d163ae9879e6262bf60
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-20 13:13:50 +0530

    updatecode

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -7,30 +7,66 @@ import {zodResolver} from '@hookform/resolvers/zod'
 function Signup() {
 
     const schema = z.object({
-       name : z.string().min(1 , "Name is required").max(40,"Name cannot exceed 40 characters") 
+       name : z.string().min(1 , "Name is required").max(40,"Name cannot exceed 40 characters"),
+       email : z.string().email('Email is required') 
     })
 
-  const { register, formState:{errors} ,handleSubmit } = useForm({resolver:zodResolver(schema)});
+  const { register , formState:{errors} ,handleSubmit } = useForm({resolver:zodResolver(schema)});
 
-console.log(errors)
+console.log(errors) ;
 
   const onSubmit = (data) => {
     console.log(data);
   };
+  
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="name">Name</label>
-        <input type="text" {...register('name')} />
-        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
-        <label htmlFor="email">Email</label>
-        <input type="email" {...register('email')} />
-        <label htmlFor="phone">Phone</label>
-        <input type="text" {...register('phone')} />
-        <label htmlFor="password">Password</label>
-        <input type="password" {...register('password')} />
-        <button type="submit">Signup</button>
-      </form>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="p-6 w-[25rem] shadow-lg rounded-md">
+        <h1 className="text-black font-bold text-3xl text-center m-2">
+          Signup
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className="block" htmlFor="Name">
+            Name
+          </label>
+          <input
+            className="input_field"
+            type="text"
+            {...register('name')}
+          />
+          <label className="block" htmlFor="Name">
+            Email
+          </label>
+          <input
+            className="input_field"
+            type="email"
+            {...register('email')}
+          />
+          <label className="block" htmlFor="Name">
+            Phone
+          </label>
+          <input
+            className="input_field"
+            type="text"
+            {...register('phone')}
+          />
+          <label className="block" htmlFor="Name">
+            Password
+          </label>
+          <input
+            className="input_field"
+            type="password"
+            {...register('password')}
+          />
+          {errors.password && <p className='text-red-500 font-semibold'>{errors.password.message}</p>}
+          <button
+            className="bg-black px-8 py-2 rounded-md w-full mt-3 text-white"
+            type="submit"
+          >
+            Signup
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
