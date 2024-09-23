93a3d45390bd16111b6718a453f393e4d9d3070e
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-23 13:47:24 +0530

    updatedCode

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -1,14 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+export const register = createAsyncThunk(
+  '/user/login',
+  async (data, { rejectWithValue }) => {
+    try {
+      const res = await axios.post('http://localhost:3000/api/register', data);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
-const initialState ={
-    loading : false ,
-    error : null
-}
+const initialState = {
+  loading: false,
+  error: null,
+};
 
 const userSlice = createSlice({
-    name : 'user',
-    initialState
-})
+  name: 'user',
+  initialState,
+  extraReducers: (builder) => {
+    //first pending
+    builder
+      .addCase(register.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(register.fulfilled, (state) => {
+        state.loading = false;
+      })
+      .addCase(register.rejected, (state, action) => {
+        console.log(action.payload);
+        state.error = action.payload;
+      });
+  },
+});
 
-export default userSlice.reducer
+export default userSlice.reducer;
